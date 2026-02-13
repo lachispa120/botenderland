a13c69372b348116c9a47d9c435484f82ac068ed
lachispa120 <lucasvidmar197@gmail.com>
2026-02-13 00:05:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -34,9 +34,9 @@ let sorteoActivo = {
 };
 
 // --- CONFIGURACIÓN TICKETS ---
-const CATEGORIA_TICKETS_ID = 'ID_AQUI'; // ID de la categoría donde se crearán los tickets
-const CANAL_LOGS_ID = 'ID_AQUI';       // ID del canal #logs-ticket
-const ROL_STAFF_ID = 'ID_AQUI';        // ID del rol de Staff que puede ver los tickets
+const CATEGORIA_TICKETS_ID = '1468130064217542750'; // ID de la categoría donde se crearán los tickets
+const CANAL_LOGS_ID = '1468150762810114118';       // ID del canal #logs-ticket
+const ROL_STAFF_ID = '1468130937681477776';        // ID del rol de Staff que puede ver los tickets
 
 // Función para consultar MCSTATUS
 async function getMCStatus() {
@@ -418,3 +418,4 @@ client.on('messageCreate', async (message) => {
 });
 
 client.login(TOKEN);
+
